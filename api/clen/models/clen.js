80eba7a9769042df002a3eba41dccd9492697923
Adam Bystran <bys.adam@gmail.com>
2020-09-05 16:24:23 +0000

    removes slug

--- api/clen/models/clen.js
+++ api/clen/models/clen.js
@@ -8,11 +8,5 @@
 const slugify = require('slugify');
 
 module.exports = {
-  beforeSave: async (model, attrs, options) => {
-    if (options.method === 'insert' && attrs.Meno) {
-      model.set('slug', slugify(attrs.Meno));
-    } else if (options.method === 'update' && attrs.Meno) {
-      attrs.slug = slugify(attrs.Meno);
-    }
-  },
-};
+	
+}
