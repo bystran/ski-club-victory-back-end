0e6a261aee716b51285d8c5bee8dc94648d79c78
Adam Bystran <bystran.adam.21@gmail.com>
2020-09-08 19:34:42 +0200

    Creates facebook api end point to get:   -latest posts   -videos   -images   -and image of the day
    -alse refactors database settings -> hides sensitive data.

--- config/database.js
+++ config/database.js
@@ -4,13 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', '0STG1hsJogUWTj75g1H4cKIKPq2q90TV'),
+        client: env('DATABASE_CLIENT', 'postgres'),
+        filename: env('DATABASE_FILENAME', ''),
+        host: env('DATABASE_HOST', ''),
+        port: env.int('DATABASE_PORT', ''),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', '' ),
         ssl: env.bool('DATABASE_SSL', false),
+
       },
       options: {}
     },
